b804765f9484e4b250016e0d2760542e1dca58be
AmanGiri007 <giriaman437@gmail.com>
2021-07-18 10:56:20 +0545

    hosts

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u6gkydpxuczjjpdu-_qg5&3-y=qz(-mg)jm)_2=ffsvb*&+w^t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['blog-begginer.herokuapp.com']
 
 
 # Application definition
